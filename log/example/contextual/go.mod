c475169d8b1fd606767b6603fbf470d3181d6280
hiraq <hiraq_mahardika@linkaja.id>
2020-12-05 11:42:57 +0700

    Added process name on measurement

--- log/example/contextual/go.mod
+++ log/example/contextual/go.mod
@@ -2,4 +2,4 @@ module github.com/quadroops/pkg/log/example/contextual
 
 go 1.15
 
-require github.com/quadroops/pkg/log v0.0.0-20201205040330-b56a56f35572
+require github.com/quadroops/pkg/log v0.0.0-20201205042642-a87da6c029ec
--- log/example/contextual/main.go
+++ log/example/contextual/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sync"
 	"time"
 
 	"github.com/quadroops/pkg/log"
@@ -11,9 +12,10 @@ func processTimes(times int) {
 	time.Sleep(time.Duration(times) * time.Second)
 }
 
-func main() {
+func process1(logger *log.ContextualLog, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	// process1
-	logger := log.Contextual(adapter.NewZerolog(), "process1")
 	logc := logger.Meta(log.KV("requestID", "random-id"), log.KV("key1", "val1"), log.KV("key2", 2))
 
 	logc.Info("Starting process...")
@@ -23,4 +25,27 @@ func main() {
 	logc.Info("Start another process...")
 	processTimes(5)
 	logc.Error("Then error...").Measure()
+}
+
+func process2(logger *log.ContextualLog, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// process 2
+	logger = logger.NewContextual("process2")
+	logger.Meta(log.KV("key1", 1), log.KV("key2", 2)).Info("Starting new process...")
+	processTimes(2)
+	logger.Info("Process success...").Info("Starting second process...")
+	processTimes(2)
+	logger.Meta(log.KV("key3", 3)).Error("Something went wrong...").Measure()
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	logger := log.Contextual(adapter.NewZerolog(), "process1")
+	go process1(logger, &wg)
+	go process2(logger, &wg)
+
+	wg.Wait()
 }
