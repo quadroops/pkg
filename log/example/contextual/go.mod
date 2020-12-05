13d900ee0972fd5d7a5e2eb40e5f49a8bda04b68
hiraq <hiraq_mahardika@linkaja.id>
2020-12-05 11:19:19 +0700

    Changed example contextual

--- log/example/contextual/go.mod
+++ log/example/contextual/go.mod
@@ -2,4 +2,4 @@ module github.com/quadroops/pkg/log/example/contextual
 
 go 1.15
 
-require github.com/quadroops/pkg/log v0.0.0-20201204161412-d388489a1458
+require github.com/quadroops/pkg/log v0.0.0-20201205040330-b56a56f35572
--- log/example/contextual/main.go
+++ log/example/contextual/main.go
@@ -14,9 +14,13 @@ func processTimes(times int) {
 func main() {
 	// process1
 	logger := log.Contextual(adapter.NewZerolog(), "process1")
-	logc := logger.Meta(log.KV("requestID", "random-id"))
+	logc := logger.Meta(log.KV("requestID", "random-id"), log.KV("key1", "val1"), log.KV("key2", 2))
 
 	logc.Info("Starting process...")
 	processTimes(2)
-	logc.Info("End porcess...")
+	logc.Info("End porcess...").Measure()
+
+	logc.Info("Start another process...")
+	processTimes(5)
+	logc.Error("Then error...").Measure()
 }
