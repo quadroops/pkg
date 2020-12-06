f9145b47812a213231d9cc7d98a1eaa139b29fa8
hiraq <hiraq_mahardika@linkaja.id>
2020-12-06 22:06:38 +0700

    Adding basic example with async and level options

--- log/example/basic/main.go
+++ log/example/basic/main.go
@@ -31,5 +31,14 @@ func main() {
 		}
 	}()
 
+	logger2 := log.New(adapter.NewZerolog(), "mylogger2",
+		log.WithOptionLevel(log.LevelWarn), log.WithAsyncEnabled())
+
+	for i := 0; i < 10; i++ {
+		logger2.Debug("This log should not be shown")
+		logger2.Info("This log should not be shown")
+		logger2.Warn("This log should be shown")
+	}
+
 	time.Sleep(10 * time.Second)
 }
